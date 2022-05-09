255e2697451855011ee332c2624857532e1e4f3f
efratz <efrat0548525405@gmail.com>
2022-05-09 22:21:13 +0300

    Add project files.

---
+++ Core/CreateFiles.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Core
+{
+    public class CreateFiles
+    {
+        string path = @"C:\Files\f";
+        Random random = new Random();
+        string s = "";
+
+        public delegate void delNew(string name);
+        public event delNew Handler_FileCreated;
+
+        public void Run()
+        {
+            Task.Factory.StartNew(Create10Files);
+        }
+
+        public void Create10Files()
+        {
+            int num;
+            s = "";
+            for (int j = 0; j < 1000; j++)
+            {
+                s += "* ";
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                num = random.Next(100, 10000);
+                File.WriteAllText($"{path}{num}.txt", s);
+            }
+        }
+
+        public void Run5Threads()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Run();
+            }
+        }
+
+
+        public async void CreateNDelete10Files()
+        {
+            s = "";
+            for (int j = 0; j < 1000; j++)
+            {
+                s += "* ";
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                string name = await CreateOneFile();
+
+                //File.Delete(path + name);         //8.3
+
+                await Task.Factory.StartNew(() =>   //9-10
+                {
+                    File.Delete(path + name);
+                });
+
+                //Task.Factory.StartNew(async() =>
+                //{
+                //    File.Delete(path + await CreateOneFile());
+                //});
+            }
+        }
+
+        public Task<string> CreateOneFile()
+        {
+            Task<string> taskRet = Task.Factory.StartNew(() =>
+            {
+                int num = random.Next(100, 10000);
+                string name = $"{num}.txt";
+                File.WriteAllText(path + name, s);
+                //Handler_FileCreated(name);
+                return name;
+            });
+            System.Threading.Thread.Sleep(2000);
+            return taskRet;
+        }
+
+
+        //public void DeleteOneFile(string name)  //del
+        //{
+        //    File.Delete(path + name);
+        //}
+    }
+}
---
+++ UI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        Core.CreateFiles createFiles;
+        public Form1()
+        {
+            InitializeComponent();
+            createFiles = new Core.CreateFiles();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createFiles.Run();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            createFiles.Run5Threads();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //createFiles.Handler_FileCreated += createFiles.DeleteOneFile;
+            createFiles.CreateNDelete10Files();
+        }
+    }
+}
